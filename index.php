480804a734d9154625b41c35348e2ff2b83c0de5
adam18092005 <adam18092005@gmail.com>
2025-01-05 19:53:40 +0530

    commited

--- index.php
+++ index.php
@@ -241,11 +241,8 @@ if(isset($_POST['openPrevG'])){
 
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-    <script src="inclusives/qbank.js"></script>
-    <script src="forall.js"></script>
+    <?php include 'inclusives/inside_body_last.html';?>
+    
     <script src="script.js"></script>
     
 </body>
